8e4be1c67eff98b861a1f10ef40c75f9bf3cbb3a
hneelabh <hneelabh13@gmail.com>
2024-07-12 15:22:48 +0530

    UI Changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gradient-to-r from-[#0F0C29] to-[#302B63]  text-gray-300 md:px-10'>
+    <div name='about' className='w-full h-screen bg-gradient-to-r from-[#0F0C29] to-[#302B63] text-gray-300 md:px-10 max-sm:px-6'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
             <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
                 <div className='sm:text-right pb-8 pl-4'>
@@ -13,7 +13,7 @@ const About = () => {
             </div>
 
             <div className='max-w-[1000px] w-full px-4 grid sm:grid-cols-2 gap-8'>
-                <div className='sm:text-right text-4xl font-bold'>
+                <div className='sm:text-right text-3xl font-bold'>
                     <p>
                         Hi. I'm Harsh, nice to meet you. Please take a look around.
                     </p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,11 +14,11 @@ import Python from '../assets/python.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full min-h-screen bg-gradient-to-r from-[#0F0C29] to-[#302B63] md:px-10 text-gray-300 py-8 pt-20'>
+    <div name='skills' className='w-full min-h-screen bg-gradient-to-r from-[#0F0C29] to-[#302B63] md:px-10 max-sm:px-6 text-gray-300 py-8 pt-20'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
-          <p className='py-4'>There are the technologies I've worked with : </p>
+          <p className='py-4'>The technologies I've worked with are : </p>
         </div>
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
